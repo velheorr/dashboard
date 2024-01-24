a5e98f5cb1f289ee585e44b6cfa5a12fdca243bb
Velheorr <velheorr@gmail.com>
2024-01-24 15:35:25 +0500

    theme fix

--- src/pages/main/subpages/Filters.jsx
+++ src/pages/main/subpages/Filters.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import {Box, FormControl, InputLabel, MenuItem, Select} from "@mui/material";
+import {palette} from "../../../utils/theme";
+import {useSelector} from "react-redux";
 
 const Filters = () => {
+    const mode = useSelector(state => state.header.mode);
+
     const [holding, setHolding] = React.useState('');
     const [zakazchik, setZakazchik] = React.useState('');
     const handleChange = (event) => {
@@ -11,14 +15,15 @@ const Filters = () => {
 
     return (
         <div>
-            <Box sx={{minWidth: 120, mb: '20px'}}>
-                <FormControl sx={{m:1, width: 300}} variant="standard">
-                    <InputLabel id="holding-label">Холдинг</InputLabel>
+            <Box sx={{minWidth: 120, mb: '20px', }}>
+                <FormControl sx={{m:1, width: 300,}} variant="standard">
+                    <InputLabel id="holding-label" sx={{color: mode === "dark" ? palette.white : palette.black}}>Холдинг</InputLabel>
                     <Select
                         labelId="holding-label"
                         id="holding"
                         value={holding}
                         onChange={handleChange}
+                        sx={{color: mode === "dark" ? palette.white : palette.black,}}
                     >
                         <MenuItem value=""><em>Пусто</em></MenuItem>
                         <MenuItem value={20}>Twenty</MenuItem>
@@ -26,12 +31,13 @@ const Filters = () => {
                     </Select>
                 </FormControl>
                 <FormControl sx={{m:1, width: 300}} variant="standard">
-                    <InputLabel id="zakazchik-label">Заказчик</InputLabel>
+                    <InputLabel id="zakazchik-label" sx={{color: mode === "dark" ? palette.white : palette.black}}>Заказчик</InputLabel>
                     <Select
                         labelId="zakazchik-label"
                         id="zakazchik"
                         value={zakazchik}
                         onChange={handleChange}
+                        sx={{color: mode === "dark" ? palette.white : palette.black}}
                     >
                         <MenuItem value=""><em>Пусто</em></MenuItem>
                         <MenuItem value={20}>Twenty</MenuItem>
