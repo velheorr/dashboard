e22ef5975a8a96ff995e4b5075bc6d8b1ac24711
Velheorr <velheorr@gmail.com>
2024-02-13 18:08:22 +0500

    +

--- src/elements/BlockShadow.jsx
+++ src/elements/BlockShadow.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import './elements.scss'
+import {useSelector} from "react-redux";
 
 const BlockShadow = ({children}) => {
+    const mode = useSelector(state => state.header.mode);
+
+
+
     return (
         <div className='blockShadow'>
             {children}
