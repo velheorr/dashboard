ca4ea15e5676c7bf4d152de9b9164280d209562e
Velheorr <velheorr@gmail.com>
2024-02-14 14:36:18 +0500

    color rules

--- src/pages/main/subpages/DetailedView.jsx
+++ src/pages/main/subpages/DetailedView.jsx
@@ -32,6 +32,80 @@ const DetailedView = () => {
 
     console.log(currentItem)
 
+    const refactorObj = {
+        АМГ: "Кряжевских А.В.",
+        ВыработкаВРублях: 129,
+        ДатаОкончанияКонтракта: "2023-12-01T00:00:00",
+        ЗапроцентованоФакт: 67680612.24,
+        КодОбъекта: "14083    ",
+        КоличествоИТРПлан: 1,
+        КоличествоИТРФакт: 1,
+        КоличествоПерсоналаПлан: 16,
+        КоличествоПерсоналаФакт: 5,
+        Контрагент: "Метафракс Кемикалс",
+        КоэфСложностиПлан: 1.02,
+        КоэфСложностиФакт: 1.91,
+        КоэфЭффективностиПлан: 1,
+        КоэфЭффективностиФакт: 0.09,
+        НаименованиеОбъекта: "2023 СМР ИТСО основной площадки",
+        НаличиеМатериалов: 54,
+        ОбъемРабот: 126,
+        ПроцентПредъявленныхРТИУ: 96,
+        ПроцентПринятыхРТИУ: 79,
+        РП: "Кряжевских Александр Владимирович",
+        СрокиКонтракта: 128,
+        СуммаКонтракта: 70770102.55,
+        Холдинг: "МЕТАФРАКС",
+    }
+
+    const buildData = (data) => {
+        const dataText = {
+            /*"Коэффициент сложности Объекта:": [КоэфСложностиФакт, КоэфСложностиПлан],*/
+            "Коэффициент сложности Объекта:": 546465465,
+            "Коэффициент эффективности:": 1,
+            "Количество персонала на объекте:": 1,
+            "Количество ИТР на объекте:": 1,
+            "Запроцентовано, руб:": 1
+        }
+        return dataText[data]
+    }
+
+
+    const formatAmount = (item) => {
+        /*return new Intl.NumberFormat("ru").format(item)*/
+        return new Intl.NumberFormat("ru", {style: "currency", currency: "RUB"}).format(item);
+    }
+    const formatColor = (fact, plan, rule = false) =>{
+        let x = (fact * 100) / plan
+        let color;
+        let y = {
+            zaprocentovano(){
+                if (x > 25) { color = 'red'}
+                else if(x > 15 && x <= 25 ) { color = 'yellow'}
+                else if(x <= 15){ color = 'green'}
+            },
+            itr(){
+                if (x <= 75 || x >= 125) { color = 'red'}
+                else if(x > 75 && x < 90 || x >= 110 && x < 125) { color = 'yellow'}
+                else if(x >= 90 && x < 110){ color = 'green'}
+            },
+            personal(){
+                if (x <= 75 || x >= 125) { color = 'red'}
+                else if(x > 75 && x < 90 || x >= 110 && x < 125) { color = 'yellow'}
+                else if(x >= 90 && x < 110){ color = 'green'}
+            },
+            effectivness(){
+                if (fact <= .69 ) { color = 'red'}
+                else if(fact > 70 && fact < .89 || fact > 1.2) { color = 'yellow'}
+                else if(fact > .9 && fact < 1.2){ color = 'green'}
+            },
+        }
+        if (rule){y[rule]()}
+        return color
+    }
+
+    formatColor(.2, 1, 'zaprocentovano')
+
     return (
         <div className='main'>
             <Button onClick={() => navigate('/')} variant="outlined" color="success" size='small' startIcon={<ArrowBackIcon />}>Назад</Button>
@@ -53,38 +127,28 @@ const DetailedView = () => {
                             gutterBottom>
                     Параметры проекта (факт / план)
                 </Typography>
-                <List>
-                    <ListItem disablePadding divider>
-                        <ListItemButton>
-                            <ListItemText primary="Коэффициент сложности Объекта:" />
-                            <ListItemText primary="54654" />
-                        </ListItemButton>
-                    </ListItem>
-                    <ListItem disablePadding divider>
-                        <ListItemButton>
-                                <ListItemText primary="Коэффициент эффективности:" />
-                        </ListItemButton>
-                    </ListItem>
-                    <ListItem disablePadding divider>
-                        <ListItemButton>
-                            <ListItemText primary="Количество персонала на объекте:" />
-                        </ListItemButton>
-                    </ListItem>
-                    <ListItem disablePadding divider>
-                        <ListItemButton>
-                            <ListItemText primary="Количество ИТР на объекте:" />
-                            <ListItemText primary="564%" />
-                        </ListItemButton>
-                    </ListItem>
-                    <ListItem disablePadding divider>
-                        <ListItemButton>
-                            <ListItemText primary="Запроцентовано, руб:" />
-                            <ListItemText primary="52 154" />
-                        </ListItemButton>
-                    </ListItem>
-                </List>
-
-
+                {currentItem?
+                    <List>
+                        <CustomItem name="Коэффициент сложности Объекта:" value={`${currentItem.КоэфСложностиФакт} | ${currentItem.КоэфСложностиПлан}`}/>
+                        <CustomItem name="Коэффициент эффективности:"
+                                    value={`${currentItem.КоэфЭффективностиФакт} | ${currentItem.КоэфЭффективностиПлан}`}
+                                    ifColor={formatColor(currentItem.КоэфЭффективностиФакт, currentItem.КоэфЭффективностиПлан, 'effectivness')}
+                        />
+                        <CustomItem name="Количество персонала на объекте:"
+                                    value={`${currentItem.КоличествоПерсоналаФакт} | ${currentItem.КоличествоПерсоналаПлан}`}
+                                    ifColor={formatColor(currentItem.КоличествоПерсоналаФакт, currentItem.КоличествоПерсоналаПлан, 'personal')}
+                        />
+                        <CustomItem name="Количество ИТР на объекте:"
+                                    value={`${currentItem.КоличествоИТРФакт} | ${currentItem.КоличествоИТРПлан}`}
+                                    ifColor={formatColor(currentItem.КоличествоИТРФакт, currentItem.КоличествоИТРПлан, 'itr')}
+                        />
+                        <CustomItem name="Запроцентовано, руб:"
+                                    value={`${formatAmount(currentItem.ЗапроцентованоФакт)} | ${formatAmount(currentItem.СуммаКонтракта)}`}
+                                    ifColor={formatColor(currentItem.ЗапроцентованоФакт, currentItem.СуммаКонтракта, 'zaprocentovano')}
+                        />
+                    </List>
+                    : <div>no data</div>
+                }
 
             </BlockShadow>
 
@@ -93,3 +157,13 @@ const DetailedView = () => {
 };
 
 export default DetailedView;
+
+const CustomItem = ({name, value, ifColor})=> {
+
+    return <ListItem disablePadding divider>
+        <ListItemButton>
+            <ListItemText primary={name} />
+            <ListItemText sx={{textAlign: 'end', color: palette[ifColor]}} primary={value} />
+        </ListItemButton>
+    </ListItem>
+}
